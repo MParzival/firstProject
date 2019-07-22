d5a06d37d09b6ac5ae10582aa68e1c7ce3ccb8fa
Mick Aubin <mick.aubin@gmail.com>
2019-07-22 16:31:24 +0200

    first commit 'apres une journée de cours Angular'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { setTheme } from 'ngx-bootstrap';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+
+  constructor() {
+    setTheme('bs4');
+  }
   title = 'firstProject';
+  firstname = 'Mickael';
+  lastname = 'Aubin';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AccordionModule } from 'ngx-bootstrap/accordion';
+import { AlertModule } from 'ngx-bootstrap/alert';
+import {PaginationModule} from 'ngx-bootstrap';
+import { MenuComponent } from './menu/menu.component';
+import { ModalModule } from 'ngx-bootstrap/modal';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AccordionModule.forRoot(),
+    AlertModule.forRoot(),
+    PaginationModule,
+    ModalModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
