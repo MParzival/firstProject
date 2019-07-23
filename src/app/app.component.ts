60b6b65f5fe08053344a62890936e41479b779cf
Mick Aubin <mick.aubin@gmail.com>
2019-07-23 11:36:26 +0200

    3eme commit 'Utilisation des Pipes'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,11 +13,26 @@ export class AppComponent {
   constructor() {
     setTheme('bs4');
   }
+
   title = 'firstProject';
   firstname = 'Mickael';
   lastname = 'Aubin';
+  todayDate = new Date();
+  number = 0.259;
 
-  clickOnMenuLink($event: string) {
-    this.h2Bar = $event + ' à cliqué sur le bouton !!';
+  object = {foo: 'bar', baz: 'qux', nested: {xyz: 3, numbers: [1, 2, 3, 4, 5]}};
+
+  // clickOnMenuLink($event: string) {
+  //   this.h2Bar = $event + ' à cliqué sur le bouton !!';
+  // }
+
+  btnClicked = false;
+  mood = 'sad';
+
+  peoples = ['Aurélien', 'Thomas', 'Pierre'];
+
+  clickOnMenuLink($event): void {
+    this.btnClicked = !this.btnClicked;
+
   }
 }
